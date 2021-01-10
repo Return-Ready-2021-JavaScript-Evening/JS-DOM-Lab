9b9a9c0ee9b4248d74dd48d9a5feb4056f5a2f75
Denae716 <denaebrown73@yahoo.com>
2021-01-10 17:12:34 -0500

    Celsius convertor

--- domlab.js
+++ domlab.js
@@ -1,13 +1,12 @@
-// f to c cnvertor function
 
-function calc(userInput){
-    userInput = document.getElementsByClassName('userInput').value
-    let c = document.getElementsByClassName('c').value
-    c += userInput - 32 * (5/9)
-    console.log(c)
+
+function calc(){
+    let userInput = parseInt(document.getElementById('userInput').value)
+    
+    let cValue = (userInput - 32) * (5/9)
+
+    document.getElementById('cValue').innerHTML = cValue
+    
+    
+    
 }
-// getting calc butten to work
-
-let button = document.getElementsByClassName('calculateButton')
-
-button[0].addEventListener('click', calc);
