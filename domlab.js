954a817e8d6f94c9c285f2ae58afaa8a3f8a49a2
Denae716 <denaebrown73@yahoo.com>
2021-01-15 16:13:50 -0500

    part2, styling needs adjusting

--- domlab.js
+++ domlab.js
@@ -1,12 +1,20 @@
 
-
+// celsius convertor
 function calc(){
     let userInput = parseInt(document.getElementById('userInput').value)
     
+    // celsius
     let cValue = (userInput - 32) * (5/9)
-
     document.getElementById('cValue').innerHTML = cValue
     
+    // kelvin
+    let kelvin = (userInput - 32) * (5/9) + 273.15
+    document.getElementById('kelvin').innerHTML = kelvin
+
+    // rankine
+    let rankine = userInput + 459.67
+    document.getElementById('rankine').innerHTML = rankine
+
     
-    
+
 }
